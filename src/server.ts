bfc54f5ca0ebbe593e425476d64eb8b4224c2d02
Hasan A <h@basri.me>
2026-03-18 04:59:27 +0300

    fix(server): support callable + fetch server entry export

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import { createStartHandler, defaultStreamHandler } from "@tanstack/react-start/server";
 
 const fetch = createStartHandler(defaultStreamHandler);
-const handler = Object.assign(fetch, { fetch });
-export { fetch };
-export default handler;
+
+const serverEntry = Object.assign(fetch, { fetch });
+
+export default serverEntry;
